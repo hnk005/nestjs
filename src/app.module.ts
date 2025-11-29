ae354d220d6688c98630d4ca7ce14d2107099606
HNK <huynhnamkha512020@gmail.com>
2025-11-29 15:36:13 +0700

    chore: apply middle logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountModule } from './count/count.module';
+import { LoggerMiddleware } from './logger.service';
 
 @Module({
   imports: [CountModule], // Add any modules you want to import here
@@ -10,4 +16,10 @@ import { CountModule } from './count/count.module';
   exports: [], // Export any modules or providers if needed
 })
 // Define and export the main application module
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
