546013bcc3b850ff43cacf0e03a1a93fe4cc01a0
HNK <huynhnamkha512020@gmail.com>
2025-11-29 14:29:33 +0700

    chore: controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // Define the controller for handling incoming requests
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Define a GET endpoint at the root path
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); // Call the service method to get the greeting message
   }
 }
