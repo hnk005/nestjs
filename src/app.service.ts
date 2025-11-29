6d73f4620df8e76bdb9807e573de8a5c54edc8c1
HNK <huynhnamkha512020@gmail.com>
2025-11-29 14:34:28 +0700

    chore: service statefull and stateless

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 
-@Injectable()
+// Statefull service to handle business logic
+// Mark as injectable so it can be injected as a dependency
+@Injectable() // scope default is singleton
 export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
+
+// Stateles service to handle business logic
+// performace slower than stateful because new instance created per request
+@Injectable({ scope: Scope.REQUEST }) // add scope to make it stateless
+export class AppServiceStateless {
   getHello(): string {
     return 'Hello World!';
   }
