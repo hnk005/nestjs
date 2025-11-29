cb50e6f18a96cd4ba2e19248d7c0d081f38ed7dd
HNK <huynhnamkha512020@gmail.com>
2025-11-29 14:29:24 +0700

    chore: module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [], // Add any modules you want to import here
+  controllers: [AppController], // Register your controllers here
+  providers: [AppService], // Register your providers here
+  exports: [], // Export any modules or providers if needed
 })
+// Define and export the main application module
 export class AppModule {}
